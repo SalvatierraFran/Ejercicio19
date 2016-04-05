d97496fe4305966c7e56ad045fb74a36d1c546d0
SalvatierraFran <franco14_93@hotmail.com>
2016-04-04 23:03:42 -0300

    Desarrollada la clase y el program según pide el ejercicio.

---
+++ Boligrafo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio19_2._0
+{
+    class Boligrafo
+    {
+        private ConsoleColor _color;
+        private int _tinta;
+
+        public Boligrafo(ConsoleColor Color, int Tinta)
+        {
+            this._color = Color;
+            this._tinta = Tinta;
+        }
+
+        public ConsoleColor getColor()
+        {
+            return this._color;
+        }
+
+        public int getTinta()
+        {
+            return this._tinta;
+        }
+
+        public bool Pintar(int gasto)
+        { }
+
+        public void Recargar()
+        { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio19_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo TintaAzul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo TintaRoja = new Boligrafo(ConsoleColor.Red, 50);
+        }
+    }
+}
